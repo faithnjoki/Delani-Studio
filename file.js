79e3d077924516aad8820eb4be3fbb5cf0caa72f
faithnjoki <faithnjoki81@gmail.com>
2021-09-15 18:27:23 +0300

    edit js

--- file.js
+++ file.js
@@ -46,29 +46,82 @@
   $("#designpic").show()
 
   })
+
+  //form
   $("#btn").click(function(){
    var name = $("input#name").val();
    alert (name + " we have received your message,Thank you for reaching out.")
   })
  })
 
- HOVER
-//   $(document).ready(function(){
-//    $("#wok1").mouseenter(function(){
-//   $("#work1").show();
-//   }, function(){
-//   $("#work1text").css("visibility", "hidden");
-// });
+ //HOVER
+$('#port1').hover(function () {
+    $(this).css("opacity", "1.7");
+    $('.p1').removeClass('text');//removeclass removes one or more class names from the selected elements
+}, function () {
+    $(this).css("opacity", "1");
+    $('.p1').addClass('text');//addclass adds one or more class names to the selected elements
+});
 
-// })
-$(document).ready(function(){
- $("#wok1").mouseenter(function () {
- $("#work1text").show();
-  })
-    .mouseleave(function () {
-      $("#work1").hide();
-    })
+$('#port2').hover(function () {
+    $(this).css("opacity", "1.7");
+    $('.p2').removeClass('text');//removeclass removes one or more class names from the selected elements
+}, function () {
+    $(this).css("opacity", "1");
+    $('.p2').addClass('text');//addclass adds one or more class names to the selected elements
+});
 
-})
+$('#port3').hover(function () {
+    $(this).css("opacity", "1.7");
+    $('.p3').removeClass('text');//removeclass removes one or more class names from the selected elements
+}, function () {
+    $(this).css("opacity", "1");
+    $('.p3').addClass('text');//addclass adds one or more class names to the selected elements
+});
+
+$('#port4').hover(function () {
+    $(this).css("opacity", "1.7");
+    $('.p4').removeClass('text');//removeclass removes one or more class names from the selected elements
+}, function () {
+    $(this).css("opacity", "1");
+    $('.p4').addClass('text');//addclass adds one or more class names to the selected elements
+});
+
+$('#port5').hover(function () {
+    $(this).css("opacity", "1.7");
+    $('.p5').removeClass('text');//removeclass removes one or more class names from the selected elements
+}, function () {
+    $(this).css("opacity", "1");
+    $('.p5').addClass('text');//addclass adds one or more class names to the selected elements
+});
+
+
+$('#port6').hover(function () {
+    $(this).css("opacity", "1.7");
+    $('.p6').removeClass('text');//removeclass removes one or more class names from the selected elements
+}, function () {
+    $(this).css("opacity", "1");
+    $('.p6').addClass('text');//addclass adds one or more class names to the selected elements
+});
+
+$('#port7').hover(function () {
+    $(this).css("opacity", "1.7");
+    $('.p7').removeClass('text');//removeclass removes one or more class names from the selected elements
+}, function () {
+    $(this).css("opacity", "1");
+    $('.p7').addClass('text');//addclass adds one or more class names to the selected elements
+});
+
+$('#port8').hover(function () {
+    $(this).css("opacity", "1.7");
+    $('.p8').removeClass('text');//removeclass removes one or more class names from the selected elements
+}, function () {
+    $(this).css("opacity", "1");
+    $('.p8').addClass('text');//addclass adds one or more class names to the selected elements
+});
+
+
+
+
 
 
